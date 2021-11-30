943da56414437b8542dc8f335709f136d992b826
Serkan Mehmet MALAGİÇ <32618577+serkanmalagic@users.noreply.github.com>
2021-11-30 10:07:21 +0300

    red, green, blue page navigation added

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwiftUI-Base-Tutorial' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SwiftUI-Base-Tutorial
+  pod 'NavigationStack'
+
+
+end
--- SwiftUI-Base-Tutorial/ContentView.swift
+++ SwiftUI-Base-Tutorial/ContentView.swift
@@ -7,7 +7,99 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RedViewOne: View {
+
+    var body: some View {
+        
+        NavigationView{
+            
+            VStack{
+                CircleView(color: .red, number: 1)
+                    .navigationBarTitle("Red one", displayMode: .inline)
+                    .offset(y: -60)
+                
+                NavigationLink(destination: BlueViewTwo(color: .blue, number: 2), label : {
+                    VStack{
+                        Text("Sonraki Sayfa")
+                            .bold()
+                            .frame(width: 280, height: 50)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        
+                    }
+                })
+            }
+        }
+        .accentColor(Color(.label))
+    }
+}
+
+struct BlueViewTwo: View {
+    
+    var color : Color
+    var number : Int
+    
+    var body: some View {
+        VStack{
+            CircleView(color: color, number: number)
+                .navigationTitle("Blue one")
+                .offset(y: -60)
+            
+            NavigationLink(destination: GreenViewThree(color: .green, number: 3), label : {
+                VStack{
+                    Text("Sonraki Sayfa")
+                        .bold()
+                        .frame(width: 280, height: 50)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    
+                }
+            })
+        }
+    }
+}
+
+struct GreenViewThree: View {
+    
+    @Environment(\.dismiss) var dismiss
+    var color : Color
+    var number : Int
+    
+    var body: some View {
+        VStack{
+            CircleView(color: color, number: number)
+                .navigationTitle("Green one")
+                .offset(y: -60)
+            
+            Button("Dismiss VC"){
+                dismiss()
+            }
+            
+        }
+    }
+}
+
+//  İçerisine veri gönderilen ve diğer sayfalardan kolayca çağırılabilen item'dir.
+struct CircleView: View {
+    
+    var color : Color
+    var number : Int
+    
+    var body: some View {
+        ZStack{
+            Circle()
+                .foregroundColor(color)
+                .frame(width: 200, height: 200, alignment: .center)
+            Text("\(number)")
+                .foregroundColor(.white)
+                .font(.system(size : 70, weight: .bold))
+        }
+    }
+}
+
+struct LoginView : View {
     
     @State var user = ""
     @State var pass = ""
@@ -15,7 +107,6 @@ struct ContentView: View {
     var body: some View {
         
         VStack{
-            
             Image("img")
             Text("SwiftUI Hoşgeldin")
                 .font(.largeTitle)
@@ -54,12 +145,7 @@ struct ContentView: View {
                         .colorInvert()
                 }
                 .myButtonStyle()
-                
-
-                
             }
-            
-            
         }.padding()
          .frame(
              minWidth: 0,
@@ -69,13 +155,12 @@ struct ContentView: View {
          .background(.white)
          .cornerRadius(25)
          .shadow(color: .purple, radius: 8, x: 1, y: 1)
-        
-        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        RedViewOne()
     }
 }
